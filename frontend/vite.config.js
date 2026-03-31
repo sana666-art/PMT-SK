29947b73d88edb62c73dd51054eae0cf7d3debe4
Sana Khalid <102246653+sana666-art@users.noreply.github.com>
2026-03-31 11:46:00 +0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '^/api/.*': {
+      '/api': {
         target: 'http://localhost:8081',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '/api'),
-        secure: false,
-      }
-    }
-  }
+        secure: false
+      },
+    },
+  },
 })
-
