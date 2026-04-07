bff1351c49d8a8472676c64e9e74061b246e2ea8
Sana Khalid <102246653+sana666-art@users.noreply.github.com>
2026-04-07 11:09:55 +0500

    Update User.java

--- backend/src/main/java/com/projectmanager/projectmanagementbackend/entity/User.java
+++ backend/src/main/java/com/projectmanager/projectmanagementbackend/entity/User.java
@@ -6,6 +6,7 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 
 @Entity
 @Table(name = "users")
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Data
 public class User {
 
     @Id
